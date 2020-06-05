fd2b39e2eae1fb394808345188b54d9531b61b6c
Rishana125 <marinka.lagunova@gmail.com>
2020-06-05 19:14:08 +0700

    Add some function fixes

--- penalty_functions_method.py
+++ penalty_functions_method.py
@@ -2,19 +2,21 @@ import math
 import numpy as np
 import scipy.optimize as op
 import sys
+import csv
 
 EPS = 1e-07
+it = 0
 
 
 def f(x):
     return (x[0] - x[1]) ** 2 + 10 * (x[0] + 5) ** 2
 
 
-def h(x):
+def g(x):
     return x[0] + x[1] - 1
 
 
-def g(x):
+def h(x):
     return - x[0] - x[1]
 
 
@@ -34,16 +36,6 @@ def g_barrier_log(value):
     return -math.log(-value) if value < 0 else 0
 
 
-def continue_searching(r, funcs, delta):
-    search = False
-    for index in range(len(r)):
-        if abs(r[index] * funcs[index]) > EPS:
-            r[index] *= delta
-            search = True
-
-    return search
-
-
 def penalty_method(x, r, funcs, delta):
     """
     Penalty/barrier method realization
@@ -56,10 +48,26 @@ def penalty_method(x, r, funcs, delta):
     Q(x, r) = f(x) + sum(rj * H(h(x))) + sum(rj * G(g(x)))
     Some functions of H and G may be missing
     """
-    while continue_searching(r, funcs(x), delta):
-        curr_min = op.fmin_powell(lambda arg: f(arg) + sum(r * funcs(arg)), x, disp=False, maxiter=100)
-        if g(curr_min) <= 0:
-            x = curr_min
+    global it
+    i = 0
+    data_array = [[i, x[0], x[1], f(x), r[0]]]
+
+    while True:
+        x_old = x.copy()
+        x = op.fmin_powell(lambda arg: f(arg) + sum(r * funcs(arg)), x, disp=False, maxiter=100)
+
+        if np.linalg.norm(x - x_old) > EPS:
+            r = np.multiply(r, delta)
+            i += 1
+            data_array.append([i, x[0], x[1], f(x), r[0]])
+        else:
+            break
+
+    with open('res{}.csv'.format(it), 'w', newline='') as file:
+        writer = csv.writer(file, delimiter=';')
+        writer.writerow(['i', 'x', 'y', 'f(x)', 'r'])
+        writer.writerows(data_array)
+    it += 1
 
     return x
 
@@ -73,14 +81,19 @@ if __name__ == "__main__":
 
     print("f(x) = (x - y)^2 + 10 * (x + 5)^2\nh(x) = x + y - 1\ng(x) = - x - y\nx initial = {}\n".format(x_init))
 
-    print("Penalty method results:\nH(x) = |x|^a\nG(x) = ((x + |x|) / 2)^a\n")
+    print("Penalty method results:\nG(x) = ((x + |x|) / 2)^a\n")
     for a in [1, 2, 4]:
-        result = penalty_method(x_init, np.ones(2), lambda arg: np.array([h_penalty(h(arg), a), g_penalty(g(arg), a)]), 1.1)
-        print("a = {}\nx = {}\nf(x) = {}\n".format(a, result, f(result)))
+        res = penalty_method(x_init, np.array([1]), lambda arg: np.array([g_penalty(g(arg), a)]), 1.1)
+        print("a = {}\nx = {}\nf(x) = {}\n".format(a, res, f(res)))
 
-    result = penalty_method(x_init, np.array([550]), lambda arg: np.array([g_barrier_inv(g(arg))]), 0.9)
+    print("Penalty method results:\nH(x) = |x|^a\n")
+    for a in [1, 2, 4]:
+        res = penalty_method(x_init, np.array([0.1]), lambda arg: np.array([h_penalty(h(arg), a)]), 2.5)
+        print("a = {}\nx = {}\nf(x) = {}\n".format(a, res, f(res)))
+
+    res = penalty_method(x_init, np.array([1]), lambda arg: np.array([g_barrier_inv(g(arg))]), 0.5)
     print("Barrier method results:\n")
-    print("G(x) = -1 / x:\nx = {}\nf(x) = {}\n".format(result, f(result)))
+    print("G(x) = -1 / x:\nx = {}\nf(x) = {}\n".format(res, f(res)))
 
-    result = penalty_method(x_init, np.array([550]), lambda arg: np.array([g_barrier_log(g(arg))]), 0.9)
-    print("G(x) = -ln(-x):\nx = {}\nf(x) = {}\n".format(result, f(result)))
+    res = penalty_method(x_init, np.array([1]), lambda arg: np.array([g_barrier_log(g(arg))]), 0.5)
+    print("G(x) = -ln(-x):\nx = {}\nf(x) = {}\n".format(res, f(res)))
