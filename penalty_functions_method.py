cdf3a08290bfd9b18fe743e6c0126f401635a2e8
Rishana125 <marinka.lagunova@gmail.com>
2020-06-19 23:54:15 +0700

    Improve data input

--- penalty_functions_method.py
+++ penalty_functions_method.py
@@ -6,7 +6,7 @@ import csv
 import warnings
 
 EPS = 1e-03
-it = 0
+func_call = 0
 
 
 def f(x):
@@ -49,53 +49,52 @@ def penalty_method(x, r, funcs, delta):
     Q(x, r) = f(x) + sum(rj * H(h(x))) + sum(rj * G(g(x)))
     Some functions of H and G may be missing
     """
-    global it
+    global func_call
     i = 0
-    data_array = [[i, x[0], x[1], f(x), r[0]]]
+    data_array = [[i, x, f(x), r]]
     with warnings.catch_warnings():
         warnings.simplefilter('ignore')
         while True:
             x_old = x.copy()
             x = op.fmin_powell(lambda arg: f(arg) + sum(r * funcs(arg)), x, disp=False, maxiter=100)
-
             if np.linalg.norm(x - x_old) > EPS:
                 r = np.multiply(r, delta)
                 i += 1
-                data_array.append([i, x[0], x[1], f(x), r[0]])
+                data_array.append([i, x, f(x), r])
             else:
                 break
 
-    with open('res{}.csv'.format(it), 'w', newline='') as file:
+    with open('res{}.csv'.format(func_call), 'w', newline='') as file:
         writer = csv.writer(file, delimiter=';')
-        writer.writerow(['i', 'x', 'y', 'f(x)', 'r'])
+        writer.writerow(['i', 'x', 'f(x)', 'r'])
         writer.writerows(data_array)
-    it += 1
+    func_call += 1
 
     return x
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print('Usage: penalty_functions_method.py [x begin] [y begin]')
+    if len(sys.argv) == 1:
+        print('Usage: penalty_functions_method.py [x begin vector]')
         sys.exit()
 
-    x_init = np.array([float(sys.argv[1]), float(sys.argv[2])])
+    x_init = np.array([float(x) for x in sys.argv[1:]])
 
     print('f(x) = (x - y)^2 + 10 * (x + 5)^2\nh(x) = x + y - 1\ng(x) = - x - y\nx initial = {}\n'.format(x_init))
 
     print('Penalty method results:\nG(x) = ((x + |x|) / 2)^a\n')
     for a in [1, 2, 4]:
-        res = penalty_method(x_init, np.array([0.1]), lambda arg: np.array([g_penalty(g(arg), a)]), 1.5)
+        res = penalty_method(x_init, np.array([1]), lambda arg: np.array([g_penalty(g(arg), a)]), 1.5)
         print('a = {}\nx = {}\nf(x) = {}\n'.format(a, res, f(res)))
 
     print('H(x) = |x|^a\n')
     for a in [1, 2, 4]:
-        res = penalty_method(x_init, np.array([0.1]), lambda arg: np.array([h_penalty(h(arg), a)]), 1.5)
+        res = penalty_method(x_init, np.array([1]), lambda arg: np.array([h_penalty(h(arg), a)]), 1.5)
         print("a = {}\nx = {}\nf(x) = {}\n".format(a, res, f(res)))
 
-    res = penalty_method(x_init, np.array([10]), lambda arg: np.array([g_barrier_inv(g(arg))]), 0.5)
+    res = penalty_method(x_init, np.array([1]), lambda arg: np.array([g_barrier_inv(g(arg))]), 0.5)
     print('Barrier method results:\n')
     print('G(x) = -1 / x:\nx = {}\nf(x) = {}\n'.format(res, f(res)))
 
-    res = penalty_method(x_init, np.array([10]), lambda arg: np.array([g_barrier_log(g(arg))]), 0.5)
+    res = penalty_method(x_init, np.array([1]), lambda arg: np.array([g_barrier_log(g(arg))]), 0.5)
     print('G(x) = -ln(-x):\nx = {}\nf(x) = {}\n'.format(res, f(res)))
